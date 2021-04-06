9cf6cf3d30927b094b9131ba52609958548e6b7d
TacticalDan <gorksorf@gmail.com>
2021-04-05 18:11:06 -0600

    tables.ts displays the total amount of money that could potentially be withdrawn to negate potential surprises

--- tables.ts
+++ tables.ts
@@ -2,10 +2,14 @@ import { accounts, allowanceTotal, flexibleExpensesTotal, inflexibleExpensesTota
 import { calculateAllowance } from "./utils";
 
 console.log("Allowances:");
-console.table(accounts.map(account => {
+const allowances = accounts.map(account => {
     const result = calculateAllowance(account);
     return { name: account.name, amount: result.quantizedAmount, days: result.daysUntilAward, hours: result.hoursUntilAward, minutes: result.minutesUntilAward, seconds: result.secondsUntilAward, };
-}));
+});
+console.table([
+    ...allowances,
+    { name: "Total", amount: Object.values(allowances).reduce((total, curr) => total + curr.amount, 0) },
+]);
 
 console.log("Accounts:");
 console.table(accounts);
